2412e3c65ebe3777f55555d8da0311314ce3148d
Zakirov Mansur <mz@dsans.com>
2020-02-04 18:06:34 +0300

    pritter fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import './assets/fonts.css';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "./assets/fonts.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
