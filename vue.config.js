753780f87217c2a2316eb2aa950edba16f6586c9
Mansur Zakirov <mansur.zakirov@boolqa.com>
2020-05-19 07:38:01 +0300

    + add best practices

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = {
   transpileDependencies: ["vuetify"],
+  productionSourceMap: false,
+  chainWebpack(config) {
+    config.plugin("CompressionPlugin").use(CompressionPlugin);
+  },
 };
